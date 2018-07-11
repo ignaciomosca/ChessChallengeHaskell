b5fec8b509facb032610bf162ab6e3261bc78a88
Ignacio Mosca <ignaciomosca@gmail.com>
2018-07-11 12:05:29 -0300

    Use record to represent piece

--- main.hs
+++ main.hs
@@ -1,28 +1,13 @@
 module Main where
 
-type Row = Int
-type Col = Int
-data ChessPiece = Rook Row Col | Bishop Row Col | Knight Row Col | Queen Row Col | King Row Col
+data PieceType = Rook | Bishop | Knight | Queen | King deriving(Show)
+data ChessPiece = Piece {row::Int, col::Int, piece::PieceType} deriving(Show)
 
 class Piece p where
-    attacks :: p -> Bool
-
-instance Show ChessPiece where
-    show (Rook _ _) = "R"
-    show (Bishop _ _) = "B"
-    show (Knight _ _) = "N"
-    show (Queen _ _) = "Q"
-    show (King _ _) = "K"
-
-rookMoves :: ChessPiece -> [(Row, Col)]
-rookMoves (Rook row col) = take (length ([ (row+x,col+y) | x <- [-1,1,0], y <-[-1,1,0]]) - 1) [ (row+x,col+y) | x <- [-1,1,0], y <-[-1,1,0]]
+    attacks :: p -> ChessPiece -> Bool
 
 instance Piece ChessPiece where
-    attacks (Rook row col) = elem (row, col) (rookMoves (Rook row col))
-    attacks (Bishop row col) = False
-    attacks (Knight row col) = False
-    attacks (Queen row col) = False
-    attacks (King row col) = False
+    attacks Piece{row = aRow, col = aCol, piece = Rook} Piece{row = r, col = c , piece = _} = aRow == r || aCol == c
 
 main = do
     putStrLn "Ahoy there! What name?"
