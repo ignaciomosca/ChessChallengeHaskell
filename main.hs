54b14e298d46ee4a6d52c5aa7902120bebe99b2d
Ignacio Mosca <ignaciomosca@gmail.com>
2018-07-12 15:10:54 -0300

    Update README

--- main.hs
+++ main.hs
@@ -13,11 +13,16 @@ class ChessBoard board where
     place :: board -> ChessPiece->Board
     done::board->Bool
 
-kingMoves::Int->Int->[(Int,Int)]
-kingMoves r c = [ (r+x,c+y) | x <- [-1,1,0], y <-[-1,1,0]]
+kingMoves :: Int -> Int -> [(Int, Int)]
+kingMoves r c = [ (r + x, c + y) | x <- [-1, 1, 0], y <- [-1, 1, 0] ]
 
-knightMoves::Int->Int->[(Int,Int)]
-knightMoves r c = [ (r+x,c+y) | x <- [-2,-1,1,2], y <-[-2,-1,1,2], abs x /= abs y]
+knightMoves :: Int -> Int -> [(Int, Int)]
+knightMoves r c =
+    [ (r + x, c + y)
+    | x <- [-2, -1, 1, 2]
+    , y <- [-2, -1, 1, 2]
+    , abs x /= abs y
+    ]
 
 instance ChessBoard Board where
     isSafe Board{usedPieces=up} c = all (\p -> not(attacks c p) && not(attacks p c)) up
@@ -38,32 +43,36 @@ instance Piece ChessPiece where
     attacks Piece{row = aRow, col = aCol, piece = Queen} Piece{row = r, col = c} = r == aRow || c == aCol || abs(r - aRow) == abs(c - aCol)
     attacks Piece{row = aRow, col = aCol, piece = King} Piece{row = r, col = c} = (r,c) `elem` (kingMoves aRow aCol)
 
-createPiece::PieceType->Int->Int->ChessPiece
-createPiece p r c = Piece{row=r, col=c, piece=p}
+createPiece :: PieceType -> Int -> Int -> ChessPiece
+createPiece p r c = Piece {row = r, col = c, piece = p}
 
-findCandidate::ChessPiece->Board->Set Board
-findCandidate Piece{row=r,col=c,piece=p} Board{m=m,n=n,usedPieces=up} = Data.Set.singleton [bs | rr <- [1..m],cc <- [1..n], let piece=createPiece(p,rr,cc), let bs = place Board{m=m,n=n,usedPieces=up} piece, isSafe bs piece ]
---findCandidate Piece{row=r,col=c,piece=p} board = (do
---    rr <- [1..8]
---    cc <- [1..8]
---    let p=createPiece(p,rr,cc)
---    let b=place board p
---    isSafe b)
+findCandidate :: ChessPiece -> Board -> Set Board
+findCandidate Piece { row = r, col = c, piece = p } Board { m = m, n = n, usedPieces = up }
+    = Data.Set.singleton
+        [ bs
+        | rr <- [1 .. m]
+        , cc <- [1 .. n]
+        , let piece = createPiece (p, rr, cc)
+        , let bs = place Board {m = m, n = n, usedPieces = up} piece
+        , isSafe bs piece
+        ]
 
-solution::Board-> [ChessPiece] -> Set Board -> Set Board
-solution board (p:ps) solutions = solution board ps (solutions `union` findCandidate p board)
+solution :: Board -> [ChessPiece] -> Set Board -> Set Board
+solution board (p : ps) solutions =
+    solution board ps (solutions `union` findCandidate p board)
 solution board [] solutions = Data.Set.filter done solutions
 
 
 main = do
     putStrLn "How many kings are to be placed on the board?"
-    kings <- getLine
+    kings <- readLn :: IO Int
     putStrLn "How many queens are to be placed on the board?"
-    queens <- getLine
+    queens <- readLn :: IO Int
     putStrLn "How many bishops are to be placed on the board?"
-    bishops <- getLine
+    bishops <- readLn :: IO Int
     putStrLn "How many rooks are to be placed on the board?"
-    rooks <- getLine
+    rooks <- readLn :: IO Int
     putStrLn "How many knights are to be placed on the board?"
-    knights <- getLine
-    putStrLn $ "Hey there  " ++ knights
+    knights <- readLn :: IO Int
+    putStrLn $ "Hey there  "
+    --show length solutions
