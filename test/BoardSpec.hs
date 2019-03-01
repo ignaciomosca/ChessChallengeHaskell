86ea263ac0b4b375c9cb946fdec4a78c258c189c
Ignacio Mosca <ignaciomosca@gmail.com>
2019-03-01 14:58:10 -0300

    Add final test

--- test/BoardSpec.hs
+++ test/BoardSpec.hs
@@ -27,3 +27,5 @@ spec = do
       length(toList(makeSolution 5 5 2 0 2 0 0)) `shouldBe` 7596
     it "6x6 2K 1B 1N 1R" $ do
       length(toList(makeSolution 6 6 2 0 1 1 1)) `shouldBe` 343816
+    it "7x7 2K 2Q 2B 0R 1N" $ do
+      length(toList(makeSolution 7 7 2 2 2 0 1)) `shouldBe` 3063828
